03399a57d06c790670389ff149ee39f2b43939d3
jennmez <jennmez@gmail.com>
2020-11-03 09:50:07 -0600

    able to pull data onto server

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
 import './App.css';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [cheese, setCheese] = useState([]);
+
+  useEffect(() => {
+    async function loadCheese() {
+      try {
+        setLoading(true);
+        const response = await axios.get('/api/products');
+        const { data } = await response;
+        console.log(data);
+        setLoading(false);
+      } catch (error) {
+        setError(error);
+        console.log('err retrieving data', error);
+      }
+    }
+    loadCheese();
+  }, []);
   return (
-    <div className="App">
-      <p>Hello</p>
-    </div>
+    <>
+      {loading && <p>Loading...</p>}
+      {error && <p>Error! Reload page please.</p>}
+      <div className="App">
+        <p>Hello</p>
+      </div>
+    </>
   );
 }
 
